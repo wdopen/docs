767b341aa25b9793fa7b823897be3c88635e50d9
lest-day <106459514+lest-day@users.noreply.github.com>
2024-11-16 11:53:41 +0800

    修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,9 +61,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'WDOPEN',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'WDOPEN Logo',
           src: 'img/logo.svg',
         },
         items: [
