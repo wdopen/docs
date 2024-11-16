11264c3c74b036c4bd9bdc6128505192ddc6415c
lest-day <106459514+lest-day@users.noreply.github.com>
2024-11-16 16:41:17 +0800

    提交

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            开始查阅文档
           </Link>
         </div>
       </div>
