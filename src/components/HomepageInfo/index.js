70598f45af7ecff0a5bae6e255365dfe3a94206f
lestday233 <106459514+lest-day@users.noreply.github.com>
2025-04-12 16:15:51 +0800

    更新 index.js

--- src/components/HomepageInfo/index.js
+++ src/components/HomepageInfo/index.js
@@ -10,7 +10,7 @@ export default function HomepageInfo() {
           <p>Wikidot是免费和付费的维基托管服务商，允许任何人创建自己的维基网站，以供分享内容、与他人协作和创建公开社群。在该平台上托管了SCP基金会、The Backrooms等知名的亚文化内容创作社群，这些社群是Wikidot中文用户的主要来源之一。Wikidot的用户们在这样的平台上建立了丰富多彩的内容，并发掘了各种特性和代码。</p>
           <img src={wikidotImg} width="75%" style={{padding: "1em 0"}} alt="Wikidot" />
           <br />
-          &#123;&nbsp;<i><a href="https://www.wikidot.com/features" class="link-instanted">点击此处了解更多有关Wikidot的信息</a></i>&nbsp;&#125;
+          <i><a href="https://www.wikidot.com/features" class="link-instanted">了解更多有关Wikidot的信息</a></i>
         </div>
       </div>
     </section>
